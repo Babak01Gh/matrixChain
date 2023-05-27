d014c5150064a2e1069c4b4c2b8a05516ce3fae6
babak-ghaffari <babakghaffarii01@gmail.com>
2023-05-28 00:15:21 +0330

    add a test of calc mults func

--- matrixChain.py
+++ matrixChain.py
@@ -1,12 +1,12 @@
 m1 = ([
-    [1,1,1],[2,2,2],[3,3,3]
-],3,3)
+    [1,1],[2,2],[3,3]
+],3,2)
 m2 = ([
     [1,1,1],[2,2,2]
 ],2,3)
 m3 = ([
     [1,1,1,1],[2,2,2,2],[3,3,3,3]
-],4,4)
+],3,4)
 w = [
     [0,0,0],
     [0,0,0],
@@ -19,22 +19,27 @@ r = [
 ]
 
 def matrixChain(matrixList):
-    for i in range(len(matrixList)):
-        for j in range(len(matrixList)):
+    n = len(matrixList)
+    for k in range(1,n):
+        for i in range(n - k):
+            j = i+k
             if i==j:
                 pass
             else:
-                muls,k = test(matrixList,i,j)
-                
+                muls,kk = test(matrixList,i,j)
+                w[i][j] = muls
+                r[i][j] = kk
+    return r[0][n-1]
                 
 def test(matrixList,i,j):
     oops = float('inf')
     retk = -1
     for k in range(i,j):
-        newVal = w[i,k] + w[k+1,j] + matrixList[i][1]*matrixList[k][1]*matrixList[j][2]
+        newVal = w[i][k] + w[k+1][j] + matrixList[i][1]*matrixList[k][2]*matrixList[j][2]
         if newVal < oops :
             oops = newVal
             retk = k
+    return (oops , retk)
         
 
-test(2,2)
+print(matrixChain([m1,m2,m3]))
