d1b65adb6b8ac55a82233b120a3f885a58b3daf2
babak-ghaffari <babakghaffarii01@gmail.com>
2023-05-28 22:39:02 +0330

    edit funcs and variables names

--- matrixChain.py
+++ matrixChain.py
@@ -1,3 +1,41 @@
+import numpy
+def minMult(matrixChain):
+    n = len(matrixChain)
+    for index in range(n):
+        w.append([0]*n)
+        r.append([0]*n)
+    for diagonal in range(1,n):
+        for i in range(n - diagonal):
+            j = i+diagonal
+            if i==j:
+                pass
+            else:
+                mults,kWithMin = minKgiver(matrixChain,i,j)
+                w[i][j] = mults
+                r[i][j] = kWithMin
+    showMults(0, n-1)
+    
+                
+def minKgiver(matrixChain,i,j):
+    firstVal = float('inf')
+    minimumK = -1
+    for k in range(i,j):
+        newVal = w[i][k] + w[k+1][j] + matrixChain[i][0]*matrixChain[k][1]*matrixChain[j][1]
+        if newVal < firstVal :
+            firstVal = newVal
+            minimumK = k
+    return (firstVal , minimumK)
+
+def showMults(i,j):
+    if i==j:
+        print(f'M{i}',end='')
+    else:
+        k = r[i][j]
+        print('(',end='')
+        showMults(i, k)
+        showMults(k+1, j)
+        print(')',end='')
+        
 chainOfMatrices = []
 numberOfMatrices = int(input('Enter the number of matrices : '))
 while numberOfMatrices:
@@ -10,38 +48,6 @@ while numberOfMatrices:
         print('There is an error in your input...')
 w,r = [],[]
 
-def matrixChain(matrixList):
-    n = len(matrixList)
-    for i in range(n):
-        w.append([0]*n)
-        r.append([0]*n)
-    for k in range(1,n):
-        for i in range(n - k):
-            j = i+k
-            if i==j:
-                pass
-            else:
-                muls,kk = test(matrixList,i,j)
-                w[i][j] = muls
-                r[i][j] = kk
-    return r[0][n-1]
-                
-def test(matrixList,i,j):
-    oops = float('inf')
-    retk = -1
-    for k in range(i,j):
-        newVal = w[i][k] + w[k+1][j] + matrixList[i][0]*matrixList[k][1]*matrixList[j][1]
-        if newVal < oops :
-            oops = newVal
-            retk = k
-    return (oops , retk)
-
-def showHow(i,j):
-    if i==j:
-        print(f'M{i}',end='')
-    else:
-        k = r[i][j]
-        print('(',end='')
-        showHow(i, k)
-        showHow(k+1, j)
-        print(')',end='')
+minMult(chainOfMatrices)
+print(numpy.array(w))
+print(numpy.array(r))
